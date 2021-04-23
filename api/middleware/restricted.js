9aea3f90eb2aa4ba6b2cbf8a733cc6007fd8b6cb
ClintFix <clint.fix@gmail.com>
2021-04-23 09:31:35 -0600

    restricted middleware done and working for jokes endpoint

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,21 @@
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../secrets');
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization;
+
+  if(!token) {
+    next({message: "token required", status: 401});
+  } else {
+    jwt.verify(token, jwtSecret, (err, decoded) => {
+      if (err) {
+        next({message: "token invalid", status: 401});
+      } else {
+        req.decodedToken = decoded;
+        next();
+      }
+    });
+  }
   /*
     IMPLEMENT
 
